391d22f151deab45129cceab31c1984330e6a4af
digitallabbd-sp3 <sp3@digitallabbd.com>
2021-10-18 14:35:35 +0600

    commited linux mvc

---
+++ LinuxTestMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinuxTestMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
